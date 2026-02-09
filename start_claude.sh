3f932741daa9bcdb686a87b6fcf36ec47cf3cac1
charketh <hvbjpyrbk3580@hotmail.com>
2026-02-09 22:27:38 +0800

    Update start_claude.sh

--- start_claude.sh
+++ start_claude.sh
@@ -2,10 +2,10 @@
 set -e
 #!/bin/bash
 
-# 设置环境变量 2月7日NCE光使用，勿动
-export ANTHROPIC_AUTH_TOKEN=sk-OTpmRgI7jPLkYXuY4f6064280641421bB8E58bAb0dB959Ba
-export ANTHROPIC_BASE_URL=https://new.aicode.us.com
-export CLAUDE_CODE_DISABLE_EXPERIMENTAL_BETAS=1
+# 设置环境变量 2月11日光传送使用，勿动
+export ANTHROPIC_AUTH_TOKEN=sk-ant-oat01-gDTowLNgyIRx2iWxf4WHwava8ls60pfu
+export ANTHROPIC_BASE_URL=https://4399code.com/claudecode
+#export CLAUDE_CODE_DISABLE_EXPERIMENTAL_BETAS=1
 # 备选API线路（去掉下面两行注释生效）
 # export ANTHROPIC_AUTH_TOKEN=sk-BnACKGNS5fruSd9Mmm2cH4MuLXGR27sB0md9KsYN6fvR4CYi
 # export ANTHROPIC_BASE_URL=https://xinghuapi.com
